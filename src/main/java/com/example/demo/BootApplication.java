1bbf26a8512a1afd15d6446dbbfc15679740fae2
maheshnagbhujange7 <maheshnagbhujange003.com>
2025-08-16 19:51:25 +0530

    fixed

--- src/main/java/com/example/demo/BootApplication.java
+++ src/main/java/com/example/demo/BootApplication.java
@@ -20,7 +20,6 @@ public class BootApplication {
 		return "Welcome to Ashok IT - Software Training";
 	}
 
-        //Bug 123
         @GetMapping("/Greet")
 	public String getWelcomeMsg() {
 		//in progress
